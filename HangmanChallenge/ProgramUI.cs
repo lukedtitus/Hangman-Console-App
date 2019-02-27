85d0b7fb0bb6cce55df34ff7e9b6f0d7d3cafbfb
lukedtitus <lukedtitus@gmail.com>
2019-02-27 16:21:44 -0500

    added hangman and guess history

--- HangmanChallenge/ProgramUI.cs
+++ HangmanChallenge/ProgramUI.cs
@@ -8,6 +8,9 @@ namespace HangmanChallenge
 {
     class ProgramUI
     {
+        public int strike = 0;
+        public string difficulty;
+
         public void Run()
         {
             RunMenu();
@@ -20,15 +23,16 @@ namespace HangmanChallenge
             Console.ReadLine();
             Console.Clear();
             string buzzWord = ChooseBuzzword();
+            Console.Clear();
             Console.WriteLine("Now, choose the difficulty for player 2.\n\t" +
                    "Easy: 10 lives\n\t" +
                    "Medium: 6 lives\n\t" +
                    "Hard: 3 lives");
-            string difficulty = Console.ReadLine().ToLower();
+            difficulty = Console.ReadLine().ToLower();
             int lives = ChooseDifficulty(difficulty);
             Console.Clear();
             Console.WriteLine("Now, give the device to player 2. Let's see if they can read your mind! :)\n\n" +
-                "Press enter to continue...");
+                "Press enter to start the game...");
             Console.ReadLine();
             RunGame(buzzWord, lives);
         }
@@ -65,23 +69,33 @@ namespace HangmanChallenge
             for (int i = 0; i < buzzWord.Length; i++)
                 displayString.Append('-');
             List<char> correctChar = new List<char>();
-            List<char> incorrectChar = new List<char>();
+            List<char> _incorrectChar = new List<char>();
+
             bool win = false;
             int lettersRevealed = 0;
-            int strike = 0;
+
 
             while (!win && strike != lives)
             {
+                DisplayHangman();
+                Console.WriteLine("Incorrect guesses:");
+                foreach (char c in _incorrectChar)
+                {
+                        Console.WriteLine($"{c}, ");
+                }
+                Console.WriteLine("\n\n");
                 Console.WriteLine(displayString.ToString());
                 Console.WriteLine("\nGuess a letter");
                 char guess = char.Parse(Console.ReadLine());
+
+                Console.Clear();
 
                 if (correctChar.Contains(guess))
                 {
                     Console.WriteLine($"You already guessed {guess} and it was correct!");
                     continue;
                 }
-                else if (incorrectChar.Contains(guess))
+                else if (_incorrectChar.Contains(guess))
                 {
                     Console.WriteLine($"You already guessed {guess} and it was incorrect :(");
                     continue;
@@ -104,7 +118,7 @@ namespace HangmanChallenge
                 else
                 {
                     Console.WriteLine($"Sorry, {guess} is not in the buzzword");
-                    incorrectChar.Add(guess);
+                    _incorrectChar.Add(guess);
                     strike++;
                 }
 
@@ -119,10 +133,405 @@ namespace HangmanChallenge
             else
             {
                 Console.WriteLine("You lost. Try again.");
+                DisplayHangman();
             }
 
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
+
+        public void DisplayHangman()
+        {
+            if (difficulty == "hard")
+            {
+                if (strike == 0)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+                else if (strike == 1)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|       0  \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+                else if (strike == 2)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|       0  \n" +
+                        "|      /|\\\n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+                else if (strike == 3)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|       0  \n" +
+                        "|      /|\\\n" +
+                        "|      / \\\n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+            }
+            else if (difficulty == "medium")
+            {
+                if (strike == 0)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+                else if (strike == 1)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|       0  \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+                else if (strike == 2)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|       0  \n" +
+                        "|       |  \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+                else if (strike == 3)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|       0  \n" +
+                        "|      /|  \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+                else if (strike == 4)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|       0  \n" +
+                        "|      /|\\\n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+                else if (strike == 5)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|       0  \n" +
+                        "|      /|\\\n" +
+                        "|      /   \n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+                else if (strike == 6)
+                {
+                    Console.WriteLine("\n" +
+                        "__________ \n" +
+                        "|       |  \n" +
+                        "|       0  \n" +
+                        "|      /|\\\n" +
+                        "|      / \\\n" +
+                        "|          \n" +
+                        "|          \n" +
+                        "|          \n\n");
+                }
+            }
+            else if (difficulty == "easy")
+            {
+                if (strike == 0)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         |  \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 1)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 2)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|        1 1 \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 3)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|        1 1 \n" +
+                        "|         1  \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 4)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|        1 1 \n" +
+                        "|         1  \n" +
+                        "|        424 \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 5)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|        1 1 \n" +
+                        "|         1  \n" +
+                        "|        424 \n" +
+                        "|       4 2 4\n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 6)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|        1 1 \n" +
+                        "|         1  \n" +
+                        "|        424 \n" +
+                        "|       4 2 4\n" +
+                        "|      4  2  4\n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 7)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|        1 1 \n" +
+                        "|         1  \n" +
+                        "|        424 \n" +
+                        "|       4 2 4\n" +
+                        "|      4  2  4\n" +
+                        "|        6 6 \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 8)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|        1 1 \n" +
+                        "|         1  \n" +
+                        "|        424 \n" +
+                        "|       4 2 4\n" +
+                        "|      4  2  4\n" +
+                        "|        6 6 \n" +
+                        "|       6   6 \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 9)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|        1 1 \n" +
+                        "|         1  \n" +
+                        "|        424 \n" +
+                        "|       4 2 4\n" +
+                        "|      4  2  4\n" +
+                        "|        6 6 \n" +
+                        "|       6   6 \n" +
+                        "|      6     6\n" +
+                        "|            \n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+                else if (strike == 10)
+                {
+                    Console.WriteLine("\n" +
+                        "____________ \n" +
+                        "|         +  \n" +
+                        "|         1  \n" +
+                        "|        1 1 \n" +
+                        "|         1  \n" +
+                        "|        424 \n" +
+                        "|       4 2 4\n" +
+                        "|      4  2  4\n" +
+                        "|        6 6 \n" +
+                        "|       6   6 \n" +
+                        "|      6     6\n" +
+                        "|     6       6\n" +
+                        "|            \n" +
+                        "|            \n\n");
+                }
+            }
+        }
     }
 }
+
+
+
+//if (incorrectChar.Count == 0)
+//                {
+//Console.WriteLine("\n" +
+//"__________ \n" +
+//"|       |  \n" +
+//"|       0  \n" +
+//"|      /|\ \n" +
+//"|      / \ \n" +
+//"|          \n" +
+//"|          \n" +
+//"|          \n");
+//                }
+//                else if (incorrectChar.Count == 1)
+//                {
+//                    Console.WriteLine("\n" +
+//                        "__________ \n" +
+//                        "|       |  \n" +
+//                        "|       0  \n" +
+//                        "|          \n" +
+//                        "|          \n" +
+//                        "|          \n" +
+//                        "|          \n" +
+//                        "|          \n");
+//                }
+//                else if (incorrectChar.Count == 2)
+//                {
+//                    Console.WriteLine("\n" +
+//                        "__________ \n" +
+//                        "|       |  \n" +
+//                        "|       0  \n" +
+//                        "|       |  \n" +
+//                        "|          \n" +
+//                        "|          \n" +
+//                        "|          \n" +
+//                        "|          \n");
+//                }
+//                else if (incorrectChar.Count == 3)
+//                {
+//                    Console.WriteLine("\n" +
+//                        "__________ \n" +
+//                        "|       |  \n" +
+//                        "|       0  \n" +
+//                        "|          \n" +
+//                        "|          \n" +
+//                        "|          \n" +
+//                        "|          \n" +
+//                        "|          \n");
+//                }
